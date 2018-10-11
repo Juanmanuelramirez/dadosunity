a0a34edba0c789c07b9a750759199608f9da528a
Tu Nombre <you@example.com>
2018-10-11 11:59:30 -0500

    first commit

---
+++ Assets/Scripts/Dice.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dice : MonoBehaviour {
+
+	Rigidbody rb;
+
+	bool hasLanded;
+	bool thrown;
+
+	Vector3 initPosition; 
+
+	public int diceValue;
+
+	public DiceSide[] diceSides;
+
+
+	void Start(){
+		rb = GetComponent<Rigidbody> ();
+		initPosition = transform.position;
+		rb.useGravity = false;
+	}
+
+	void Update(){
+
+
+		if(Input.GetKeyDown(KeyCode.Mouse0)){
+			RollAgain ();
+		}
+
+		if (rb.IsSleeping () && !hasLanded && thrown) {
+			hasLanded = true;
+			rb.useGravity = false;
+
+			SideValueCheck ();
+		}
+	}
+
+	void RollDice (){
+		
+		if (!thrown && !hasLanded) {
+			thrown = true;
+			rb.useGravity = true;
+			rb.AddTorque (Random.Range (0, 500), Random.Range (0, 500), Random.Range (0, 500));
+		} else if (thrown && hasLanded) {
+			Reset ();
+		}
+	}
+
+	void Reset (){
+		
+		transform.position = initPosition;
+		thrown = false;
+		hasLanded = false;
+		rb.useGravity = false;
+	}
+
+	void RollAgain(){
+		
+		Reset ();
+		thrown = true;
+		rb.useGravity = true;
+		rb.AddTorque (Random.Range (0, 500), Random.Range (0, 500), Random.Range (0, 500));
+	}
+
+
+	void SideValueCheck(){
+		diceValue = 0;
+
+		foreach (DiceSide side in diceSides) {
+			if (side.OnGround ()) {
+				diceValue = side.sideValue;
+				Debug.Log ("Has tirado " + diceValue);
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelLoader : MonoBehaviour {
+
+	public GameObject loadingScreen;
+	public Slider slider;
+	public Text progressText;
+
+	void Start() {
+		StartCoroutine (LoadAsynchronously());
+	}
+
+
+	IEnumerator LoadAsynchronously () {
+		AsyncOperation operation = SceneManager.LoadSceneAsync (1);
+		loadingScreen.SetActive (true);
+		while (!operation.isDone) {
+			float progress = Mathf.Clamp01 (operation.progress / 0.9f);
+
+			slider.value = progress;
+            progressText.text = string.Format("{0:0}%", progress * 100);
+
+			Debug.Log(progress);
+
+			yield return null;
+		}
+	}
+}
